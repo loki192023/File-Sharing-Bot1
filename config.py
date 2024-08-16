037d4843640037c5b6b4c14d87e83761cf60d3d2
loki192023 <134685561+loki192023@users.noreply.github.com>
2024-08-17 03:20:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,23 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hi {first}\n\nIam File Share Bot For Tamil 247
-You Need Join Our All Channel To Access The Files.
-
-Channel 1: 
-@Tamil247Entertainments1
-
-Channel 2:
-https://t.me/+6r7IWdqBj0ZmMTFl
-
-Channel 3:
-@tamil24x7movierequest
-
-Channel 4:
-https://t.me/+6t85ORZ7Fa0xYTBl
-
-Channel 5:
-https://t.me/+9EIE9e8YANA3YjQ9")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -62,23 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hi {first}\n\nIam File Share Bot For Tamil 247
-You Need Join Our All Channel To Access The Files.
-
-Channel 1: 
-@Tamil247Entertainments1
-
-Channel 2:
-https://t.me/+6r7IWdqBj0ZmMTFl
-
-Channel 3:
-@tamil24x7movierequest
-
-Channel 4:
-https://t.me/+6t85ORZ7Fa0xYTBl
-
-Channel 5:
-https://t.me/+9EIE9e8YANA3YjQ9")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
